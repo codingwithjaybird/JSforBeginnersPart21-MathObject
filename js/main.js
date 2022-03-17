85c32f8e11bbda54e0c759fdbe935f553ab01f56
Jaya <jayasigot@gmail.com>
2022-03-17 17:46:21 -0400

    Initial Upload

---
+++ js/main.js
@@ -0,0 +1,35 @@
+// MATH OBJECT:
+
+// Math.PI
+console.log(Math.PI);
+
+let area = Math.PI * 2 * 2;
+console.log(area);
+
+// Math.round()
+let areaRounded = Math.round(area);
+console.log(areaRounded);
+
+let avg = 86.7273;
+console.log(avg);
+
+// Math.floor()
+let avgFloor = Math.floor(avg);
+console.log(avgFloor);
+
+// Math.ceil()
+let avgCeiling = Math.ceil(avg);
+console.log(avgCeiling);
+
+// Math.round()
+let randomNumber = Math.random();
+console.log(randomNumber);
+
+
+// Math.sqrt(n);
+let sqrt = Math.sqrt(9);
+console.log(sqrt);
+
+// Math.pow(x,y)
+let powerNum = Math.pow(2,4);
+console.log(powerNum);
